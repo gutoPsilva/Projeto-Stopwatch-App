a4c1c677b01030619aabf71fea60a4207956a718
gutoPsilva <60670589+gutoPsilva@users.noreply.github.com>
2023-05-16 00:15:31 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Projeto-Stopwatch-App
+ Simples projeto de relógio digital desenvolvido de forma autônoma.
---
+++ script.js
@@ -0,0 +1,25 @@
+const horaAtualElement = document.querySelector('.hora-atual');
+
+// atualizar relógio a cada segundo.
+setInterval(() =>{
+  let d = new Date();
+  const horaAtual = {
+    hora: d.getHours(),
+    minuto: d.getMinutes(),
+    segundo: d.getSeconds()
+  }
+
+  // converter para 2 digitos caso esteja entre 0-9 --> para ser 00-09
+  horaAtual.hora    = twoDigits(horaAtual.hora);
+  horaAtual.minuto  = twoDigits(horaAtual.minuto);
+  horaAtual.segundo = twoDigits(horaAtual.segundo);
+
+  horaAtualElement.innerHTML = `${horaAtual.hora}:${horaAtual.minuto}:${horaAtual.segundo}`
+}, 1000);
+
+function twoDigits(elementoHora){
+  if(elementoHora < 10){
+    return '0' + elementoHora;
+  }
+  return elementoHora;
+}
