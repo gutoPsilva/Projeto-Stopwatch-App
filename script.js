2a54c11412d05a3394dd8092a0b4dc877aaecaf1
gutoPsilva <60670589+gutoPsilva@users.noreply.github.com>
2023-05-16 00:26:54 -0300

    adjusts

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 const horaAtualElement = document.querySelector('.hora-atual');
 
 // atualizar relógio a cada segundo.
-setInterval(() =>{
+setInterval(() => atualizarHorario(), 1000);
+
+function atualizarHorario(){
   let d = new Date();
   const horaAtual = {
     hora: d.getHours(),
     minuto: d.getMinutes(),
     segundo: d.getSeconds()
-  }
+  };
 
   // converter para 2 digitos caso esteja entre 0-9 --> para ser 00-09
-  horaAtual.hora    = twoDigits(horaAtual.hora);
-  horaAtual.minuto  = twoDigits(horaAtual.minuto);
+  horaAtual.hora = twoDigits(horaAtual.hora);
+  horaAtual.minuto = twoDigits(horaAtual.minuto);
   horaAtual.segundo = twoDigits(horaAtual.segundo);
 
-  horaAtualElement.innerHTML = `${horaAtual.hora}:${horaAtual.minuto}:${horaAtual.segundo}`
-}, 1000);
+  horaAtualElement.innerHTML = `${horaAtual.hora}:${horaAtual.minuto}:${horaAtual.segundo}`;
+}
 
 function twoDigits(elementoHora){
   if(elementoHora < 10){
