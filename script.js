9bd28c8f46745a2e41b7ea572b038ae9968dcb02
gutoPsilva <60670589+gutoPsilva@users.noreply.github.com>
2023-05-17 16:50:02 -0300

    err0r

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const horaAtualElement = document.querySelector('.hora-atual');
-
+const horaAtualElement = document.querySelector('.js-hora-atual');
 // atualizar relógio a cada segundo.
 setInterval(() => atualizarHorario(), 1000);
 
@@ -20,8 +19,34 @@ function atualizarHorario(){
 }
 
 function twoDigits(elementoHora){
-  if(elementoHora < 10){
-    return '0' + elementoHora;
+  return elementoHora < 10 ? '0' + elementoHora : elementoHora; // retornar elementoHora com 0X caso for menor que 10
+}
+
+const playButtonElement = document.getElementById("js-play-button");
+const timerStatusElement = document.getElementById('timSts');
+
+document.body.addEventListener('keyup', (event) => {
+  if(event.key === 'p'){
+    playPause();
   }
-  return elementoHora;
+});
+
+playButtonElement.addEventListener('click', playPause);
+
+function playPause(){
+  if(playButtonElement.innerText === 'play_arrow'){
+    playButtonElement.innerHTML = `<span class="material-icons md-36">pause</span>`;
+    timerStatusElement.innerText = `Active`;
+    timerStatusElement.style.color = '#5FEAD1';
+    document.getElementById('play-icon').style.color = "#000000";
+    playButtonElement.classList.add('active-button');
+  }
+  else{
+    playButtonElement.innerHTML = `<span class="material-icons md-36">play_arrow</span>`;
+    timerStatusElement.innerText = `Inactive`;
+    timerStatusElement.style.color = '#FBFBFB';
+    document.getElementById('play-icon').style.color = "red"
+    playButtonElement.classList.remove('active-button');
+    
+  }
 }
