6035ffa7f7933ef263205d98e6fa884688ba0be8
gutoPsilva <60670589+gutoPsilva@users.noreply.github.com>
2023-05-20 15:14:19 -0300

    complete

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const playButtonElement = document.getElementById("js-play-button");
 playButtonElement.addEventListener('click', playPauseTimer);
 
 const stopButtonElement = document.getElementById("js-stop-button");
-stopButtonElement.addEventListener('click', stopTimer);
+stopButtonElement.addEventListener('click', restartAll);
 
 const saveButtonElement = document.getElementById("js-save-button");
 saveButtonElement.addEventListener('click', saveLap);
@@ -15,7 +15,7 @@ document.body.addEventListener('keyup', (e) => {
     playPauseTimer();
   }
   else if(e.key === 's'){
-    stopTimer();
+    restartAll();
   }
   else if(e.key === 'l'){
     saveLap();
@@ -63,9 +63,7 @@ function playPauseTimer(){
   alterStylesOnPlay();
 }
 
-let didClean = false;
-
-function stopTimer(){
+function restartAll(){
   clearInterval(intervalID);
   isPlaying = false;
 
@@ -76,7 +74,6 @@ function stopTimer(){
   min = 0;
   sec = 0;
   ms = 0;
-  didClean = true;
   displayTimer(ms, sec, min);
 
   playButtonElement.innerHTML = `<span class="material-icons md-36">play_arrow</span>`;
@@ -84,6 +81,9 @@ function stopTimer(){
   timerStatusElement.style.color = '#FBFBFB';
   playButtonElement.classList.remove('active-button');
   document.querySelector('.circle-container').style.boxShadow = "0px 0px 15px 2px rgba(0, 0, 0, 0.6)";
+  savedRunsElement.style.opacity = 0;
+  savedRunsElement.innerHTML = '';
+  i = 1;
 }
 
 function alterStylesOnPlay(){
@@ -111,24 +111,21 @@ function displayTimer(ms, sec, min){
   document.querySelector('.timer-min').innerHTML = twoDigits(min);
 }
 
+const savedRunsElement = document.querySelector('.run-saves');
+
 let i = 1;
 function saveLap(){
-  const savedRunsElement = document.querySelector('.run-saves');
+
   if(!min && !sec && !ms){
     alert('There are no values to save.');
     return;
   }
-
-  if(didClean){
-    savedRunsElement.innerHTML = '';
-    savedRunsElement.style.opacity = 1;
-    didClean = false;
-  }
-
+  
+  savedRunsElement.style.opacity = 1;
 
   if(i>6){
     alert('You have reached the limit of 6 laps.');
-    const msg = confirm('Do you want to clean the lap list?');
+    const msg = confirm('Do you want to clean the lap list and save this new lap?');
     if(msg){
       savedRunsElement.innerHTML = '';
       i = 1;
